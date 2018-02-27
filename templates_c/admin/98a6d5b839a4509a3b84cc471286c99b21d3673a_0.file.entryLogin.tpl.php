4d9b864b11064770fb3c6a1f158e232fd14836d2
Daniel Štorc <dokanscze@gmail.com>
2018-02-27 21:21:51 +0100

    edit

--- templates_c/admin/98a6d5b839a4509a3b84cc471286c99b21d3673a_0.file.entryLogin.tpl.php
+++ templates_c/admin/98a6d5b839a4509a3b84cc471286c99b21d3673a_0.file.entryLogin.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.31, created on 2018-02-27 19:10:21
+/* Smarty version 3.1.31, created on 2018-02-27 20:12:12
   from "D:\wamp64\www\gybuliga\templates\admin\entryLogin.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.31',
-  'unifunc' => 'content_5a95ad1d799506_53144863',
+  'unifunc' => 'content_5a95bb9c19f533_46739857',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '98a6d5b839a4509a3b84cc471286c99b21d3673a' => 
     array (
       0 => 'D:\\wamp64\\www\\gybuliga\\templates\\admin\\entryLogin.tpl',
-      1 => 1519758617,
+      1 => 1519762324,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5a95ad1d799506_53144863 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5a95bb9c19f533_46739857 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <html>
 <head>
@@ -35,7 +35,7 @@ function content_5a95ad1d799506_53144863 (Smarty_Internal_Template $_smarty_tpl)
 <?php echo '<script'; ?>
  rel="script" src="js/jquery-ui.js"><?php echo '</script'; ?>
 >
-<form method="post" action="/gadmin/">
+<form method="post" action="http://gybuliga.dev/gadmin/">
     <div class="box">
         <input type="text" name="[login]username" class="email" placeholder="Přihlašovací jméno">
         <input type="password" name="[login]password" class="password" placeholder="Heslo">
